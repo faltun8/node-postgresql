5e17cb7fed71f072015f5bade131446648e5e402
furkan <frknaltn08@gmail.com>
2020-12-15 18:01:52 -0500

    project complated

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const usersController = require("../controllers/usersController");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/*  /users */
+
+router.get("/", usersController.get_users);
+
+router.get("/add", usersController.show_add_user_form);
+router.post("/add", usersController.add_user);
+
+router.get("/:id/delete", usersController.delete_user);
+
+router.get("/:id/edit", usersController.show_edit_user_page)
+router.post("/:id/edit", usersController.edit_user)
 
 module.exports = router;
